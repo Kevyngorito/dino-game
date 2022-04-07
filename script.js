0e4e54ac6a41529e5b24d7628130cf8cdac5bb53
Kevyn Gorito <kevyndccap@gmail.com>
2022-04-07 13:53:07 -0300

    Corrigindo bug de pular no ar DINO

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const dino = document.querySelector('.dino');
+let isJumping = false;
 
 function handleKeyUp(event){
 if (event.keyCode === 32) {
+    if (!isJumping){
     jump();
+        }   
     }
 }
 
 function jump () {
     let position = 0;
+    isJumping = true;
 
     let upInterval = setInterval (() => {
         if(position >= 150) {
             clearInterval(upInterval);
-            //descendo
-           let downInterval = setInterval (() => {
-               if(position <= 0) {
-                   clearInterval(downInterval);
-               } else{
-               position -= 20;
-               dino.style.bottom = position + 'px';
-               }
-           }, 20);
+
+                //descendo
+                let downInterval = setInterval (() => {
+                    if(position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                    } else{
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                    }
+                }, 20);
         } else {
-        //subindo
-        position += 20;
-        dino.style.bottom = position + 'px';
-        }
+                //subindo
+                position += 20;
+                dino.style.bottom = position + 'px';
+            }
     },20);
 }
 
