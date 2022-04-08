27c4b3b8f14bc769020f8a9b857423f78195537b
Kevyn Gorito <kevyndccap@gmail.com>
2022-04-08 11:32:16 -0300

    Inserção de background-image para containers e criação de animação de fundo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const dino = document.querySelector('.dino');
+const mario = document.querySelector('.mario');
 const background = document.querySelector('.background');
 let isJumping = false;
 let position = 0;
@@ -26,49 +26,49 @@ function jump () {
                     isJumping = false;
                     } else{
                     position -= 20;
-                    dino.style.bottom = position + 'px';
+                    mario.style.bottom = position + 'px';
                     }
                 }, 20);
         } else {
                 //subindo
                 position += 20;
-                dino.style.bottom = position + 'px';
+                mario.style.bottom = position + 'px';
             }
     },20);
 }
 
-function createCactus() {
-    const cactus = document.createElement('div');
-    let cactusPosition = 1000;
+function createBala() {
+    const bala = document.createElement('div');
+    let balaPosition = 1100;
     let randomTime = Math.random() * 6000;
 
     console.log(randomTime);
     
 
-    cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
-    background.appendChild(cactus);
+    bala.classList.add('bala');
+    bala.style.left = 1300 + 'px';
+    background.appendChild(bala);
 
     let leftInterval = setInterval(() => {
         
-        if(cactusPosition < -60) {
+        if(balaPosition < -60) {
             clearInterval(leftInterval);
-            background.removeChild(cactus);
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            background.removeChild(bala);
+        } else if (balaPosition > 0 && balaPosition < 60 && position < 60) {
             // Game over
 
             clearInterval(leftInterval);
             document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         } else {
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
+            balaPosition -= 10;
+            bala.style.left = balaPosition + 'px';
 
         }
     }, 20);
 
-    setTimeout(createCactus, randomTime);
+    setTimeout(createBala, randomTime);
 }
 
 
-createCactus();
+createBala();
 document.addEventListener('keyup', handleKeyUp);
